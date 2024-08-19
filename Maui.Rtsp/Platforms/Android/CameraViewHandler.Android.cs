d536e73da264f67d40fccb5eaf19c086186cd4cc
Ricardo R <rretamal.dev@gmail.com>
2024-08-19 08:25:48 -0400

    Initial files

---
+++ Maui.Rtsp/Platforms/Android/CameraViewHandler.Android.cs
@@ -0,0 +1,28 @@
+﻿using Android.Content;
+using Android.Views;
+using Maui.Rtsp.Controls;
+using Microsoft.Maui.Handlers;
+
+namespace Maui.Rtsp.Handlers
+{
+    public class CameraViewHandler : ViewHandler<ICameraView, SurfaceView>
+    {
+        public static IPropertyMapper<ICameraView, CameraViewHandler> Mapper = new PropertyMapper<ICameraView, CameraViewHandler>(ViewHandler.ViewMapper)
+        {
+            [nameof(ICameraView.Url)] = MapUrl
+        };
+
+        public CameraViewHandler() : base(Mapper)
+        {
+        }
+
+        protected override SurfaceView CreatePlatformView()
+        {
+            return new SurfaceView(Android.App.Application.Context);
+        }
+
+        public static void MapUrl(CameraViewHandler handler, ICameraView view)
+        {
+        }
+    }
+}
---
+++ Maui.Rtsp/Platforms/iOS/CameraViewHandler.iOS.cs
@@ -0,0 +1,32 @@
+﻿#if IOS
+using AVKit;
+using Maui.Rtsp.Controls;
+using Microsoft.Maui.Handlers;
+using UIKit;
+
+namespace Maui.Rtsp.Handlers
+{
+    public class CameraViewHandler : ViewHandler<ICameraView, UIView>
+    {
+        public static IPropertyMapper<ICameraView, CameraViewHandler> Mapper = new PropertyMapper<ICameraView, CameraViewHandler>(ViewHandler.ViewMapper)
+        {
+            [nameof(ICameraView.Url)] = MapUrl
+        };
+
+        public CameraViewHandler() : base(Mapper)
+        {
+        }
+
+        protected override UIView CreatePlatformView() => new UIView();
+
+        protected override void ConnectHandler(UIView platformView)
+        {
+            base.ConnectHandler(platformView);
+        }
+
+        public static void MapUrl(CameraViewHandler handler, ICameraView view)
+        {
+        }
+    }
+}
+#endif
